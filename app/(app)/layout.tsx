8dfe291b7a388089dc952c4cece5bf29bd718fbd
viettt76 <viethq@deepsync.vn>
2025-12-12 10:20:57 +0700

    [footer] footer component

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Footer from '@/components/shared/footer';
 import Header from '@/components/shared/header';
 import InitLoadingProvider from '@/providers/init-loading-provider';
 import { ReactNode } from 'react';
@@ -9,6 +10,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
         <InitLoadingProvider>
             <Header />
             {children}
+            <Footer />
         </InitLoadingProvider>
     );
 }
---
+++ components/shared/footer.tsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+    return (
+        <div className="bg-primary mt-4">
+            <div className="max-w-screen-xl mx-auto">
+                <div className="w-full pt-4 pb-6 grid grid-cols-4 gap-x-4 text-white">
+                    <div className="flex flex-col gap-y-1">
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi Về chúng tôi</span>
+                    </div>
+                    <div className="flex flex-col gap-y-1">
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                    </div>
+                    <div className="flex flex-col gap-y-1">
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                    </div>
+                    <div className="flex flex-col gap-y-1">
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                        <span>Về chúng tôi</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
